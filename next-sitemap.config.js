fa6e4dad62dd24b963222080ce432111e2b4054f
amine boulila <amin.boulila@gmail.com>
2026-01-24 16:33:52 +0100

    added sitmap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL, // or your custom domain
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: ['/api/*'],
+};
